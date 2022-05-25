471a1b1aef981c7eb564605c211633fc36835d0b
Brian Winkers <bwinkers@gmail.com>
2022-05-25 03:26:01 -0700

    Use the shared FS module for Flow creation w Electron

--- editor-ui/src-electron/electron-preload.js
+++ editor-ui/src-electron/electron-preload.js
@@ -64,97 +64,13 @@ contextBridge.exposeInMainWorld("electronApi", {
     }
   },
 
-  getElectronNuggetById: async (rootDir, nuggetId) => {
+  createFlow: async (flow) => {
     try {
-      console.log("GET - Nugget ID: " + nuggetId);
-
-      // The parent directory that we expect to find Flows defined in sub-directories.
-      const nuggetDir = rootDir + osSep + "nuggets" + osSep + nuggetId + osSep;
-
-      try {
-        const nugget = readJson([nuggetDir], "nugget");
-        return nugget;
-      } catch (e) {
-        console.log(e);
-        return null;
-      }
+      const flowData = await createFlow(flow);
+      return flowData;
     } catch (e) {
       console.log(e);
       return null;
-    }
-  },
-
-  getFlowData: async (rootDir, flowId, dataType) => {
-    try {
-      console.log("GET - FlowData for ID: " + flowId);
-
-      // The parent directory that we expect to find Flows defined in sub-directories.
-      const flowDir = rootDir + osSep + "flows" + osSep + flowId;
-
-      const flowDataResult = await readJson([flowDir], dataType);
-      if (flowDataResult.status != "success") {
-        return null;
-      }
-
-      return flowDataResult.data;
-    } catch (e) {
-      console.log("ERROR CAUGHT");
-      console.log(e);
-    }
-  },
-
-  getJsonMulti: async (rootDir, type, idArray) => {
-    const objects = [];
-
-    await Promise.all(
-      idArray.map(async (objectId) => {
-        console.log(objectId);
-        let readResult = {};
-        switch (type) {
-          case "nugget":
-            readResult = await readJson(
-              [rootDir, "nuggets", objectId],
-              "nugget"
-            );
-            break;
-          case "flow":
-            readResult = await readJson([rootDir, "flows", objectId], "flow");
-            break;
-        }
-
-        if (readResult.status && readResult.status === "success") {
-          objects.push(readResult.data);
-        }
-      })
-    );
-
-    console.log("getJsonMulti");
-    console.log(objects);
-
-    return objects;
-  },
-
-  deleteJson: async (dirs = [], fileName, fileData) => {
-    try {
-      const dirPath = dirs.join(osSep);
-      const fullPath = dirPath + osSep + fileName + ".json";
-      unlinkSync(fullPath);
-
-      return { status: "success", deleted: fullPath };
-    } catch (e) {
-      console.log(e);
-      return { status: "failure" };
-    }
-  },
-
-  deleteDir: async (dirs = []) => {
-    try {
-      const dirPath = dirs.join(osSep);
-      rmSync(dirPath, { recursive: true });
-      return { status: "success", deleted: dirPath };
-    } catch (e) {
-      console.log(e);
-      return { status: "failure" };
     }
   },
 });
