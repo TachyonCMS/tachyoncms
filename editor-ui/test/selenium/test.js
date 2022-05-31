e0d234b7b34212a62efaf33cdd704deed5817f3d
Brian Winkers <bwinkers@gmail.com>
2022-05-30 19:07:37 -0700

    WiP for Selenium

---
+++ editor-ui/test/selenium/test.js
@@ -0,0 +1,22 @@
+const webdriver = require("selenium-webdriver");
+const driver = new webdriver.Builder()
+  // The "9515" is the port opened by ChromeDriver.
+  .usingServer("http://localhost:9515")
+  .withCapabilities({
+    "goog:chromeOptions": {
+      // Here is the path to your Electron binary.
+      binary:
+        "/home/brian/sandbox/tachyoncms/editor-ui/dist/electron/TachyonCMS-linux-x64/TachyonCMS",
+    },
+  })
+  .forBrowser("chrome") // note: use .forBrowser('electron') for selenium-webdriver <= 3.6.0
+  .build();
+driver.get("http://www.google.com");
+driver.findElement(webdriver.By.name("q")).sendKeys("webdriver");
+driver.findElement(webdriver.By.name("btnG")).click();
+driver.wait(() => {
+  return driver.getTitle().then((title) => {
+    return title === "webdriver - Google Search";
+  });
+}, 1000);
+driver.quit();
