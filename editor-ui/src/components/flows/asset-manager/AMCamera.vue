9155380ddb43a0a069aaef85c50fae34f01e573c
Brian Winkers <bwinkers@gmail.com>
2022-06-14 08:02:44 -0700

    WiP

--- editor-ui/src/components/flows/asset-manager/AMCamera.vue
+++ editor-ui/src/components/flows/asset-manager/AMCamera.vue
@@ -1,23 +1,186 @@
 <template>
-  <div class="row col-12">Camera</div>
+  <div class="row col-12 text-center justify-center items-center">
+    <div class="videobox">
+      <multi-corder
+        ref="multicorder"
+        :video-source="videoSource"
+        :videoTypes="videoTypes"
+        :recorderMode="recorderMode"
+        @error="onError"
+        @cameras="onCameras"
+        @video-live="onVideoLive"
+        @view-change="onViewChange"
+        @new-recording="onNewRecording"
+        @delete-recording="onDeleteRecording"
+        @player-loaded="onPlayerLoaded"
+      ></multi-corder>
+    </div>
+    {{ videoSourceList }}
+  </div>
 </template>
 
 <script>
-import { defineComponent, ref, computed } from "vue";
+import { defineComponent, ref, onMounted } from "vue";
 
-import useFlows from "../../../composables/useFlows";
+// This provides the shared code for the HTML5 Recording.
+import MultiCorder from "./MultiCorder";
 
+/**
+ * Wraps Multicorder in a Camera specific UI.
+ * This is useful for adding a camera to any blocks display or editor component.
+ * The camera is designed to save the data to the Containing Nugget.
+ */
 export default defineComponent({
   name: "AssetManagerCamera",
-  props: ["nuggetId"],
-  setup(props) {
+  emits: ["cameras", "error", "notification"],
+  components: {
+    MultiCorder,
+  },
+  props: {
+    recorderMode: {
+      type: String,
+      default: "single",
+    },
+  },
+  setup(props, { emit }) {
     console.log(props);
-    const { nuggetAssetMap } = useFlows();
-    const tabView = ref("files");
-    const nuggetAssets = computed(() => {
-      return nuggetAssetMap.get(props.nuggetId);
-    });
-    return { nuggetAssets };
+
+    const controls = ref(null);
+    const videoSource = ref(null);
+    const videoSourceList = ref([]);
+    const isPaused = ref(false);
+    const isPlayerPaused = ref(false);
+    const isMuted = ref(true);
+    const isPlayerMuted = ref(true);
+    const view = ref("video");
+    const recordings = ref([]);
+
+    return {
+      controls,
+      videoSource,
+      videoSourceList,
+      isPaused,
+      isPlayerPaused,
+      isMuted,
+      isPlayerMuted,
+      view,
+      recordings,
+    };
+  },
+  methods: {
+    onError(error) {
+      console.log("Error emitted", error);
+    },
+    onCameras(cameras) {
+      console.log("Available cameras", cameras);
+      /**
+       * We are implementing a `multicorder` with camera and screen support.
+       * We need to create a list that groups the items for a `v-select` component.
+       * We use the `listFromCameras` helper function provided by the component.
+       * The Multicorder component maintains a list of `cameras` if we need them independently.
+       */
+      this.videoSourceList = this.$refs.multicorder.listFromCameras(cameras);
+    },
+    onVideoLive() {
+      this.controls = "liveVideo";
+    },
+    onViewChange(view) {
+      this.view = view;
+    },
+    onNewRecording(recording) {
+      this.recordings.push(recording);
+      if (this.recorderMode == "single") {
+        // Load the video into the player and force disposition
+        // this.view = "videoPlayer";
+        this.loadRecording(0);
+      }
+    },
+    onDeleteRecording(index) {
+      this.recordings.splice(index, 1);
+      if (this.recorderMode == "single") {
+        this.controls = "liveVideo";
+      }
+    },
+    onPlayerLoaded() {
+      //this.playRecording();
+    },
+    videoRecord() {
+      this.controls = "recordingVideo";
+      this.$refs.multicorder.startVideoRecording();
+    },
+    videoSnapshot(fromView) {
+      this.$refs.multicorder.videoSnapshot(fromView);
+    },
+    videoClose() {
+      this.$refs.multicorder.stopVideo();
+      this.view = "video";
+      this.controls = "liveVideo";
+      this.videoSource = null;
+    },
+    videoStopRecording() {
+      this.$refs.multicorder.stopRecording();
+      // resume the video, minus recording
+      this.resume();
+    },
+    resume() {
+      this.isPaused = false;
+      this.$refs.multicorder.resume();
+    },
+    pause() {
+      this.isPaused = true;
+      this.$refs.multicorder.pause();
+    },
+    closeSnapshot() {
+      this.$refs.multicorder.closeSnapshot();
+    },
+    snapshotDownload() {
+      this.$refs.multicorder.downloadSnapshot();
+    },
+    downloadRecording(index) {
+      if (this.recorderMode === "single") {
+        index = 0;
+      }
+      this.$refs.multicorder.downloadRecording(index);
+    },
+    deleteRecording(index) {
+      if (this.recorderMode === "single") {
+        index = 0;
+      }
+      this.$refs.multicorder.deleteRecording(index);
+    },
+    async loadRecording(index) {
+      await this.$refs.multicorder.loadRecording(index);
+    },
+    playRecording() {
+      this.isPlayerPaused = false;
+      this.$refs.multicorder.playRecording();
+    },
+    pausePlayer() {
+      this.isPlayerPaused = true;
+      this.$refs.multicorder.pausePlayer();
+    },
+    resumePlayer() {
+      this.isPlayerPaused = false;
+      this.$refs.multicorder.resumePlayer();
+    },
+    deletePlayerRecording() {
+      this.$refs.multicorder.deletePlayerRecording();
+    },
+    closePlayer() {
+      this.$refs.multicorder.closePlayer();
+    },
+    toggleMuted() {
+      this.isMuted = !this.isMuted;
+    },
+    togglePlayerMuted() {
+      this.isPlayerMuted = !this.isPlayerMuted;
+    },
   },
 });
 </script>
+
+<style scoped>
+.videobox {
+  background-color: black;
+}
+</style>
--- editor-ui/src/components/flows/blocks/editors/MulticorderEditor.vue
+++ editor-ui/src/components/flows/blocks/editors/MulticorderEditor.vue
@@ -1,113 +1,22 @@
 <template>
   <div class="row col-12">
-    Multicorder
-    <div class="row col-12">
-      <heading :data="editorData"></heading>
-    </div>
-    <div class="row col-12">
-      <q-input
-        outlined
-        v-model="editorData.heading"
-        :label="label"
-        class="col-12"
-        :data-cy="dataCySlug + '-heading-fld'"
-      ></q-input>
-      <!-- Control buttons -->
-      <div class="row col-12">
-        <q-space></q-space>
-        <q-btn
-          @click="this.clear()"
-          class="subdued-btn"
-          icon-right="mdi-sync"
-          flat
-          size="sm"
-          label="Clear"
-          :disabled="!dirtyBit"
-          :data-cy="dataCySlug + '-clear-btn'"
-        ></q-btn>
-        <q-btn
-          @click="
-            this.$emit('save', { newData: editorData });
-            this.dirtyBit = false;
-          "
-          class="option-btn"
-          icon="save"
-          size="sm"
-          :disabled="!dirtyBit"
-          label="Save"
-          :data-cy="dataCySlug + '-save-btn'"
-        ></q-btn>
-        <q-space></q-space>
-        <q-btn
-          @click="this.$emit('close')"
-          class="subdued-btn"
-          icon-right="mdi-close"
-          flat
-          size="sm"
-          label="Close"
-          :disabled="dirtyBit"
-          :data-cy="dataCySlug + '-close-btn'"
-        ></q-btn>
-      </div>
-    </div>
+    <am-camera></am-camera>
   </div>
 </template>
 
 <script>
-import { defineComponent, unref, ref, watch, reactive, computed } from "vue";
-import { useQuasar } from "quasar";
+import { defineComponent, ref } from "vue";
 
-import Heading from "../renders/HeadingBlock";
+import AmCamera from "../../asset-manager/AMCamera";
 
 export default defineComponent({
   name: "MulticorderEditor",
   props: ["data", "dataCySlug"],
-  emits: ["save", "close", "delete"],
   components: {
-    Heading,
+    AmCamera,
   },
   setup(props) {
-    console.log("HEADING EDITOR");
-    const $q = useQuasar();
-
-    // We know this to be a shallow object so this should provide a non-reactive copy.
-    const rawData = Object.assign({}, props.data);
-
-    // Create a local reactive object, not linked to the props.
-    const editorData = reactive(rawData);
-
-    // Set to true, if the data has changed
-    const dirtyBit = ref(false);
-
-    // Undo any UNSAVED changes
-    const clear = () => {
-      console.log("Clearing");
-      editorData.heading = props.data.heading;
-      dirtyBit.value = false;
-    };
-
-    // Watch for changes and update the dirtyBit accordingly
-    watch(editorData, (value) => {
-      if (value && value.heading != props.data.heading) {
-        dirtyBit.value = true;
-      } else {
-        // Catches the user manually undoing the change
-        dirtyBit.value = false;
-      }
-    });
-
-    // Define the label for the field
-    const label = computed(() => {
-      return "H" + editorData.level + " Heading";
-    });
-
-    // Expose these to the rest of the script
-    return {
-      dirtyBit,
-      label,
-      clear,
-      editorData,
-    };
+    console.log("Multicorder Capture");
   },
 });
 </script>
