f85640c92826283fe9aed3c2aafefbf36b4159ff
Brian Winkers <bwinkers@gmail.com>
2022-09-27 01:56:00 -0700

    Starting point

---
+++ cloud-app/src/boot/amplify.js
@@ -0,0 +1,3 @@
+import { Amplify } from "aws-amplify";
+import awsExports from "../aws-exports";
+Amplify.configure(awsExports);
---
+++ cloudapp/src/pages/IndexPage.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
+
+import Amplify from "aws-amplify";
+import awsconfig from "../aws-exports";
+
+Amplify.configure(awsconfig);
+</script>
+
+<template>
+  <authenticator>
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+    </template>
+  </authenticator>
+</template>
