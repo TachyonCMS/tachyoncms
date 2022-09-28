188411ff66aed15fff44d0bd91ca10d5175e28ae
Brian Winkers <bwinkers@gmail.com>
2022-09-28 03:03:42 -0700

    Prepping for shared code

--- cloudapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ cloudapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,18 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "userPoolGroups": {
-            "adminGroupRole": "string",
-            "techGroupRole": "string"
-        },
-        "cloudappdevviteauth": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string",
-            "CreatedSNSRole": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}
